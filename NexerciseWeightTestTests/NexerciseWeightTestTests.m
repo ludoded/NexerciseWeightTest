f3856e00ff8db281c7d56e67ed2bd19fe8ec34cb
Haik <haik.ampardjian@gmail.com>
2014-04-04 17:46:16 +0400

    first commit

---
+++ NexerciseWeightTestTests/NexerciseWeightTestTests.m
@@ -0,0 +1,34 @@
+//
+//  NexerciseWeightTestTests.m
+//  NexerciseWeightTestTests
+//
+//  Created by Aik Ampardjian on 03.04.14.
+//  Copyright (c) 2014 Boomie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NexerciseWeightTestTests : XCTestCase
+
+@end
+
+@implementation NexerciseWeightTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,2 @@
+pod 'AFNetworking', '~> 2.2.1'
+pod 'BEMSimpleLineGraph', '~> 2.0'
